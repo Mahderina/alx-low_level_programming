7e0d8a2ed0c269d5901ad1aee5137e49710ab69b
Mahderina <111346013+Mahderina@users.noreply.github.com>
2022-09-13 00:47:58 +0100

    coommitt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 
 /**
-  *  main - Function that print a text
-  * Return:  0
-  */
 * main - Function that print a text.
 * Return:  0.
 */
@@ -11,5 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-i
 }
