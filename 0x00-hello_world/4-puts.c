1a18f1ba03f78830b60d3cb305becb90152d5e3f
Mahderina <111346013+Mahderina@users.noreply.github.com>
2022-09-12 22:58:35 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+  *  main - Function that print a text
+  * Return:  0
+  */
+* main - Function that print a text.
+* Return:  0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+i
+}
