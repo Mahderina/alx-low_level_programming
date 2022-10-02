6d98e9c0376a5c3e2053e994c2e718af1fc3740d
Mahderina <111346013+Mahderina@users.noreply.github.com>
2022-10-02 17:29:48 +0300

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,16 +1,36 @@
 /**
- * _strlen - count the length of string
+ * _strncpy - A function that copies a string.
  *
- * @s: pointer to char that takes string input
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
  *
- * Return: string length
+ * Return: @dest
 */
 
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0';)
+	/**
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	/**
+	 * if the length of source is less than n
+	 * write additional nullbytes to dest to
+	 * ensure that a total of n bytes is written
+	*/
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
-	return (i);
+	}
+
+	return (dest);
 }
